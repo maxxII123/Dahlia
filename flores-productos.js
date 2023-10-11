d5e124a679b25ac1eb3c4613262055db5c5c9321
maximiliano <maximiliano.serey.antilef@alumnos.sip.cl>
2023-10-10 22:26:06 -0300

    10/10/23

--- flores-productos.js
+++ flores-productos.js
@@ -1,39 +1,65 @@
+
 const flores = [
     {
         id:1,
         nombre: "tulipan",
-        img: "./img/tulipan3.jpg",
+        img: "/img/lirio1.jpg",
         precio: 999,
+        descripcion:"edkfejfefjafa",
     },
     {
         id:2,
         nombre:"rosa",
-        img: "./img/rosa1.jpg",
+        img: "/img/rosa1.jpg",
         precio: 999,
+        descripcion:"edkfejfefjafa",
     },
     {
         id:3,
         nombre:"margarita",
         img:"/img/margaritas2.jpg",
         precio: 500,
+        descripcion:"edkfejfefjafa",
     },
     {
         id:4,
         nombre: "girasol",
         img: "/img/girasoles2.jpg",
         precio:876,
+        descripcion:"edkfejfefjafa",
     },
     {
         id:5,
         nombre:"dahlias",
-        img: '/img/dahlis2.jpg',
+        img: '/img/dalias2.jpg',
         precio:578,
+        descripcion:"edkfejfefjafa",
     },
     {
         id:6,
         nombre:"ramo",
         img:"/img/ramo2.jpg",
         precio:845,
+        descripcion:"edkfejfefjafa",
     },
+    {
+        
+    }
 
 ]
+
+
+
+var container = document.getElementById("container-card");
+for (let i = 0; i < 10; i++) {
+    const element = flores[i];
+container.innerHTML+=`
+<div class='product-card'>
+<img src='`+element.img+`'>
+<h1>`+ element.nombre +`</h1>
+<p>` + element.descripcion +` </p>
+<p>` + element.precio + `</p>
+<button class="buy-button">Comprar</button>
+</div>`;
+    
+}   

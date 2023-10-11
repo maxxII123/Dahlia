965698ac75e272fb106f1cb9b3f96757bd7f78e0
maximiliano <maximiliano.serey.antilef@alumnos.sip.cl>
2023-10-11 00:03:31 -0300

    BOOMM

--- flores-productos.js
+++ flores-productos.js
@@ -3,42 +3,42 @@ const flores = [
     {
         id:1,
         nombre: "tulipan",
-        img: "/img/lirio1.jpg",
+        imag: "/img/lirio1.jpg",
         precio: 999,
         descripcion:"edkfejfefjafa",
     },
     {
         id:2,
         nombre:"rosa",
-        img: "/img/rosa1.jpg",
+        imag: "/img/rosa1.jpg",
         precio: 999,
         descripcion:"edkfejfefjafa",
     },
     {
         id:3,
         nombre:"margarita",
-        img:"/img/margaritas2.jpg",
+        imag:"/img/margaritas2.jpg",
         precio: 500,
         descripcion:"edkfejfefjafa",
     },
     {
         id:4,
         nombre: "girasol",
-        img: "/img/girasoles2.jpg",
+        imag: "/img/girasoles2.jpg",
         precio:876,
         descripcion:"edkfejfefjafa",
     },
     {
         id:5,
         nombre:"dahlias",
-        img: '/img/dalias2.jpg',
+        imag: '/img/dalias2.jpg',
         precio:578,
         descripcion:"edkfejfefjafa",
     },
     {
         id:6,
         nombre:"ramo",
-        img:"/img/ramo2.jpg",
+        imag:"/img/ramo2.jpg",
         precio:845,
         descripcion:"edkfejfefjafa",
     },
@@ -48,18 +48,46 @@ const flores = [
 
 ]
 
-
+var carrito = [];
 
 var container = document.getElementById("container-card");
 for (let i = 0; i < 10; i++) {
     const element = flores[i];
-container.innerHTML+=`
+container.innerHTML+=` 
 <div class='product-card'>
-<img src='`+element.img+`'>
+<img src="`+ element.imag +`">
 <h1>`+ element.nombre +`</h1>
 <p>` + element.descripcion +` </p>
 <p>` + element.precio + `</p>
-<button class="buy-button">Comprar</button>
+<button class="buy-button" onclick="agregarAlCarrito(${element.id})">Comprar</button>
 </div>`;
     
 }   
+
+function agregarAlCarrito(id) {
+    const florSeleccionada = flores.find(flor => flor.id === id);
+    carrito.push(florSeleccionada);
+    mostrarCarrito();
+}
+
+function mostrarCarrito() {
+    var carritoContainer = document.getElementById("carrito-container");
+    carritoContainer.innerHTML = "";
+
+    var total = 0;
+    carrito.forEach(flor => {
+        total += flor.precio;
+        carritoContainer.innerHTML += `<div>${flor.nombre} - ${flor.precio} CPL</div>`;
+    });
+
+    carritoContainer.innerHTML += `<div>Total: ${total} CPL</div>`;
+}
+
+
+
+
+
+
+
+
+
